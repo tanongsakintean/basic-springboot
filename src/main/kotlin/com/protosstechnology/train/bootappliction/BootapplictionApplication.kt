106c4ab22592b3c93d966abd40bd5dbb2cd2ce67
Tanongsak Intean <71477852+tanongsakintean@users.noreply.github.com>
2024-04-22 11:47:02 +0700

    first commit

---
+++ src/main/kotlin/com/protosstechnology/train/bootappliction/BootapplictionApplication.kt
@@ -0,0 +1,11 @@
+package com.protosstechnology.train.bootappliction
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BootapplictionApplication
+
+fun main(args: Array<String>) {
+	runApplication<BootapplictionApplication>(*args)
+}
---
+++ src/test/kotlin/com/protosstechnology/train/bootappliction/BootapplictionApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.protosstechnology.train.bootappliction
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BootapplictionApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
